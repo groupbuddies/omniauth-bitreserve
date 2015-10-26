dfcede803c6c47eb63d183ffc57113d11d17ddce
Miguel Palhas <mpalhas@gmail.com>
2015-10-26 11:26:16 +0000

    Fixes compatibility with bitreserve

--- lib/omniauth/strategies/uphold.rb
+++ lib/omniauth/strategies/uphold.rb
@@ -5,8 +5,8 @@ module OmniAuth
     class Uphold < ::OmniAuth::Strategies::OAuth2
       include ::OmniAuth::Strategy
 
-      UPHOLD_URL = ENV['UPHOLD_URL'] || 'https://uphold.com'
-      UPHOLD_API_URL = ENV['UPHOLD_API_URL'] || 'https://api.uphold.com'
+      UPHOLD_URL = ENV['UPHOLD_URL'] || ENV['BITRESERVE_URL'] || 'https://uphold.com'
+      UPHOLD_API_URL = ENV['UPHOLD_API_URL'] || ENV['BITRESERVE_API_URL'] || 'https://api.uphold.com'
 
       option :name, :uphold
       option :client_options, {
